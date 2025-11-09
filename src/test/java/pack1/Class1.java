347e8579dc27c6c486be6a94243eceab0090be64
Sangeetha <sangee.pushpa3@gmail.com>
2025-11-09 10:19:35 +0530

    Third message

--- src/test/java/pack1/Class1.java
+++ src/test/java/pack1/Class1.java
@@ -5,7 +5,7 @@ public class Class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("How are you");
+		System.out.println("How are yo now");
 		
 	}
 
